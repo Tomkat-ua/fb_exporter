e7fd512f63bb6c8e1555f4b805790a5fcbe702b3
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-08-15 09:20:35 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+
+from prometheus_client import start_http_server, Info, Gauge
+from dotenv import load_dotenv
+import db,time
+
+load_dotenv()
+
+app_name = 'Firebird Exporter'
+app_ver = '0.0.1'
+http_server_port = 5000
+sql = os.getenv('SQL')
+
+fb_monitoring = Gauge('fb_monitoring','FB Monitoring Data',['param','db'])
+info= Info('app','Application about',['name','version'])
+
+def get_data():
+    try:
+        con = db.get_connection()
+        cur = con.cursor()
+        cur.execute(sql)
+        result = cur.fetchall()
+        con.commit()
+        cur.close()
+        con.close()
+        return result
+    except Exception as e:
+        return str(e)
+
+info.labels(app_name, app_ver)
+
+if __name__ == "__main__":
+    get_data()
+    print('Start metrics server on http://localhost:'+str(http_server_port))
+    start_http_server(http_server_port)
+    info.labels(app_name, app_ver)
+    while True:
+        # try:
+        #     fb_monitoring.labels('reload proc', db.db_path).set(0)
+            for row in get_data():
+                fb_monitoring.labels(row[0], db.db_path).set(row[1])
+            time.sleep(15)
+        # except Exception as e:
+        #     fb_monitoring.labels('reload proc', db.db_path).set(1)
